6687450bda2c2dc1d14904b20e77108444323d98
Sudeeeep <sudeepn15@gmail.com>
2023-05-30 14:55:23 +0530

    add header

---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+import logo from "../assets/logo.svg";
+import searchLogo from "../assets/search_FILL1_wght400_GRAD0_opsz48.svg";
+
+const Header = () => {
+  return (
+    <>
+      <header className="min-[580px]:flex justify-between my-3 ">
+        <img src={logo} alt="windbnb logo" />
+        <div className="flex items-center m-6 min-[580px]:m-0">
+          <input
+            className="mr-0.5 p-1 border-2 rounded-s-lg "
+            placeholder="Add location"
+            type="text"
+          />
+          <input
+            className="mr-0.5 p-1 border-2"
+            placeholder="Add guests"
+            type="text"
+          />
+          <button className="border-2 rounded-e-lg outline-none">
+            <img src={searchLogo} className=" w-8 h-8  p-1" alt="search" />
+          </button>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
